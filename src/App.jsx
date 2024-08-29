d7af851b97110bd9063857fd7cdee757bb396aa2
=LC Thanh <lcthanh.htvn@gmail.com>
2024-08-29 15:17:28 +0700

    Setup dark/light mode with extendTheme and CssVarsProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import Button from '@mui/material/Button'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      variant="outlined"
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+
       <h1>Test</h1>
       <Button variant="contained">Hello world</Button>
       <Button variant="outlined">Outlined</Button>
--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,18 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { red, teal } from '@mui/material/colors'
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: red
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
+    dark: {
+      palette: {
+        primary: teal
+      }
     }
   }
 })
