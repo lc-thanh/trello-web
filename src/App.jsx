107a2d523c2edcd97d1b96eedd420ed61d46d1a3
=LC Thanh <lcthanh.htvn@gmail.com>
2024-08-30 14:50:05 +0700

    Create Light/Dark/System ModeSelect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
 import Button from '@mui/material/Button'
 import { useColorScheme } from '@mui/material/styles'
 
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+    // setAge(event.target.value);
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="mode-select-label">Mode</InputLabel>
+      <Select
+        labelId="mode-select-label"
+        id="mode-select"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+        <MenuItem value="system">System</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
   return (
@@ -19,6 +50,8 @@ function App() {
 
   return (
     <>
+      <ModeSelect />
+      <br />
       <ModeToggle />
 
       <h1>Test</h1>
