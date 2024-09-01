d3235272a11b50bb8f2e3266560d6d4158e5867f
=LC Thanh <lcthanh.htvn@gmail.com>
2024-09-01 17:40:32 +0700

    Done AppBar (not responsive)

---
+++ src/components/AppBar/Menus/Workspaces.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+
+function Workspaces() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <Box>
+      <Button
+        id="basic-button-workspaces"
+        aria-controls={open ? 'basic-menu-workspaces' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        endIcon={<ExpandMoreIcon />}
+      >
+        Workspaces
+      </Button>
+      <Menu
+        id="basic-menu-workspaces"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-workspaces'
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Workspaces
--- src/theme.js
+++ src/theme.js
@@ -1,21 +1,58 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
-import { red, teal } from '@mui/material/colors'
+import { blueGrey, teal, deepOrange, orange } from '@mui/material/colors'
 
-// Create a theme instance.
 const theme = extendTheme({
   trello: {
-    appBarHeight: '48px',
-    boardBarHeight: '58px'
+    appBarHeight: '58px',
+    boardBarHeight: '60px'
   },
   colorSchemes: {
     light: {
       palette: {
-        primary: red
+        primary: teal,
+        secondary: deepOrange
       }
     },
     dark: {
       palette: {
-        primary: teal
+        primary: blueGrey,
+        secondary: orange
+      }
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem',
+          '.MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.light
+          },
+          '&:hover': {
+            '.MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.main
+            }
+          }
+          // '& fieldset': {
+          //   borderWidth: '1px !important'
+          // }
+        })
       }
     }
   }
